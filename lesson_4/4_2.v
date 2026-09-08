0dfd953da1cff5bcb8ea40fa0d2932c07457e5a9
embed-logic <yurii.melnychuk+embed@gmail.com>
2026-09-08 12:12:25 +0300

    Add 2-to-1 multiplexer module
    Implement a 2-to-1 multiplexer module with a case statement for selection.

---
+++ lesson_4/4_2.v
@@ -0,0 +1,15 @@
+module mux2to1 (
+   input  wire a,
+   input  wire b,
+   input  wire sel,
+   output reg  y
+);
+
+   always @(*) begin
+       case (sel)
+           1'b0: y = a;
+           // 1'b1 навмисно відсутній
+       endcase
+   end
+
+endmodule
